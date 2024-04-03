77ae217dd3c0e43a78cf89a4d9a0f1fe15742016
Max <scorpiomax90@gmail.com>
2024-04-03 18:36:47 +0300

    change logo

--- components/layouts/Sidebar.vue
+++ components/layouts/Sidebar.vue
@@ -1,9 +1,9 @@
 
 import type { NuxtImg } from '#build/components';
 <template>
-	<aside>
-		<NuxtLink to="/">
-			<NuxtImg src="/logo.png" width="100" height="100" alt="logo crm" />
+	<aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+		<NuxtLink to="/" class="mb-10 block">
+			<NuxtImg class="mx-auto" src="/logo.svg" width="100" height="100" alt="logo crm" />
 		</NuxtLink>
 	</aside>
 </template>
