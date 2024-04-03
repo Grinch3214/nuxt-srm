ceacf97d58b79a7e3478a7bbcfdf5f8354b0f1e9
Max <scorpiomax90@gmail.com>
2024-04-04 01:24:05 +0300

    added constants and appwrites

---
+++ app.constants.ts
@@ -0,0 +1,6 @@
+export const APP_WRITE_ID = '660dc62a22e3a71c03f1'
+export const DB_ID = 'crm-base'
+export const COLLECTION_DEALS = 'deals'
+export const COLLECTION_CUSTOMERS = 'customers'
+export const COLLECTION_COMMENTS = 'comments'
+export const STORAGE_ID = 'storage'
