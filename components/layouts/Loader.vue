a60f3f94d82de7b79edd00ca8eb16c10dfe96311
Max <scorpiomax90@gmail.com>
2024-04-04 00:06:50 +0300

    create loader

---
+++ components/layouts/Loader.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="wrapper">
+		<img src="/loader.svg" alt="loader" width="500" height="500">
+	</div>
+</template>
+
+<style scoped>
+	.wrapper {
+		@apply bg-sidebar flex items-center justify-center w-screen h-screen;
+	}
+</style>
--- components/layouts/Sidebar.vue
+++ components/layouts/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-	<aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+	<aside class="px-5 py-8 bg-sidebar h-full relative w-full shadow-md">
 		<NuxtLink to="/" class="mb-10 block">
 			<NuxtImg class="mx-auto" src="/logo.svg" width="100" height="100" alt="logo crm" />
 		</NuxtLink>
