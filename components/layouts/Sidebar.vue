71cc596a181efedf97e83e344aed8bf2ce2b3fe0
Max <scorpiomax90@gmail.com>
2024-04-03 18:01:27 +0300

    create layouts/added nuxt image module

---
+++ components/layouts/Sidebar.vue
@@ -0,0 +1,13 @@
+
+import type { NuxtImg } from '#build/components';
+<template>
+	<aside>
+		<NuxtLink to="/">
+			<NuxtImg src="/logo.png" width="100" height="100" alt="logo crm" />
+		</NuxtLink>
+	</aside>
+</template>
+
+<script lang="ts" setup>
+
+</script>
