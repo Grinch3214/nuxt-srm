8aeea3f572f547ae2af8ddbb231cbccf76eb6f43
Max <scorpiomax90@gmail.com>
2024-04-03 18:10:22 +0300

    added tailwindcss

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-	<section class="grid">
+	<section class="grid grid-cols-[1fr_6fr]">
 		<LayoutsSidebar />
 		<div>
 
@@ -11,11 +11,3 @@
 <script lang="ts" setup>
 
 </script>
-
-<style scoped>
-	.grid {
-		min-height: 100vh;
-		display: grid;
-		grid-template-columns: 1fr 6fr;
-	}
-</style>
